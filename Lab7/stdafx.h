1636acf602c80216bf7a22c84f1e4b5557dbf8d5
Stinni <kristinnf13@ru.is>
2016-03-11 02:29:30 +0000

    added Lab7 and started working on it!

---
+++ Lab7/stdafx.h
@@ -0,0 +1,12 @@
+#pragma once
+// Ogre headers:
+#include "OGRE/OgreRoot.h"
+#include "OGRE/OgreSceneManager.h"
+#include "OGRE/OgreSceneNode.h"
+#include "OGRE/OgreRenderWindow.h"
+#include "OGRE/OgreConfigFile.h"
+#include "OGRE/OgreEntity.h"
+#include "OGRE/OgreWindowEventUtilities.h"
+#include "OGRE/OgreMeshManager.h"
+#include "OGRE/OgreMath.h"
+#include "OIS/OIS.h"
